faa7a88e2345eba60249dbdfffb20d60223f5337
Peter Smith <Peter Smith>
2016-03-07 17:55:09 -0400

    use a more yii style rest call

--- config.php
+++ config.php
@@ -4,6 +4,12 @@ namespace humhub\modules\api;
 return [
     'id' => 'api',
     'class' => 'humhub\modules\api\Module',
-    'namespace' => 'humhub\modules\api'
+    'namespace' => 'humhub\modules\api',
+    'urlManagerRules' => [
+        ['class' => 'humhub\modules\space\components\UrlRule'],
+      	'user' => 'api/user',
+      	'user/<id:\d+>' => 'api/user/view',
+      	'user/search/<search:.+>' => 'api/user/search'
+    ],
 ];
 ?>
