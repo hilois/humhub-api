5d2d82f7a63aacc80ec96cadf71b3bcf50270da7
Peter Smith <Peter Smith>
2016-03-15 15:10:48 -0300

    make the admin api user list more humhub-y

---
+++ widgets/ApiUserMenu.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace humhub\modules\api\widgets;
+
+use Yii;
+use yii\helpers\Url;
+use humhub\models\Setting;
+
+/**
+ * ApiUser Administration Menu
+ *
+ * @author petersmithca
+ */
+class ApiUserMenu extends \humhub\widgets\BaseMenu
+{
+
+    public $template = "@humhub/widgets/views/tabMenu";
+    public $type = "apiUserSubNavigation";
+
+    public function init()
+    {
+
+        $this->addItem(array(
+            'label' => 'Overview',
+            'url' => Url::toRoute(['/api/admin/index']),
+            'sortOrder' => 100,
+            'isActive' => (Yii::$app->controller->module && Yii::$app->controller->module->id == 'api' && Yii::$app->controller->id == 'admin' && Yii::$app->controller->action->id == 'index'),
+        ));
+        $this->addItem(array(
+            'label' => 'Add new Api User',
+            'url' => Url::toRoute(['/api/admin/create']),
+            'sortOrder' => 200,
+            'isActive' => (Yii::$app->controller->module && Yii::$app->controller->module->id == 'api' && Yii::$app->controller->id == 'admin' && Yii::$app->controller->action->id == 'add'),
+        ));
+
+        parent::init();
+    }
+
+}
