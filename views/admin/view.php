0e6f81c0caf86bd3a4812453febf3989415fd8a8
Peter Smith <Peter Smith>
2016-03-17 08:01:06 -0300

    Fixes #13 - api user view

--- views/admin/view.php
+++ views/admin/view.php
@@ -5,34 +5,15 @@ use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
 /* @var $model humhub\modules\api\models\ApiUser */
-
-$this->title = $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Api Users', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="api-user-view">
+<div class="panel panel-default">
+    <div class="panel-heading"><?php echo '<strong>View</strong> Api User'; ?></div>
+    <div class="panel-body">
+        <p />
 
-    <h1><?= Html::encode($this->title) ?></h1>
+        <?php $form = \yii\widgets\ActiveForm::begin(); ?>
+        <?php echo $hForm->render($form); ?>
+        <?php \yii\widgets\ActiveForm::end(); ?>
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'client',
-            'api_key',
-            'active',
-        ],
-    ]) ?>
-
+    </div>
 </div>
